8fe2a833c2831b667ace433f459d78975e05434d
Kevin Luke <78250732+lukekevin@users.noreply.github.com>
2023-09-29 17:01:17 +0530

    Update video_editor.py

--- video_editor.py
+++ video_editor.py
@@ -1,13 +1,18 @@
-import argparse
 from moviepy.editor import *
 
-def clip_videos(file_list, start_time, end_time):
+def clip_videos(file_list):
+    print('Enter start and end times for clipping:')
+    start_time = float(input("Start time (in seconds): "))
+    end_time = float(input("End time (in seconds): "))
+    
     for file in file_list:
         video = VideoFileClip(file)
         clipped = video.subclip(start_time, end_time)
         clipped.write_videofile(file[:-4] + "_clipped.mp4")
 
-def resize_videos(file_list, height_val):
+def resize_videos(file_list):
+    height_val = int(input('Enter the height for resizing: '))
+    
     for file in file_list:
         video = VideoFileClip(file)
         resized = video.resize(height=height_val)
@@ -17,7 +22,9 @@ def merge_videos(file_list):
     merged = concatenate_videoclips([VideoFileClip(file) for file in file_list])
     merged.write_videofile('merged.mp4')
 
-def flip_videos(file_list, flip_angle):
+def flip_videos(file_list):
+    flip_angle = int(input('Enter the flip angle (in degrees): '))
+    
     for file in file_list:
         video = VideoFileClip(file)
         flipped = video.rotate(flip_angle)
@@ -30,33 +37,28 @@ def mute_videos(file_list):
         muted.write_videofile(file[:-4] + "_muted.mp4")
 
 def main():
-    parser = argparse.ArgumentParser(description="Simple video editor using MoviePy.")
-    parser.add_argument("operation", choices=["clip", "resize", "merge", "flip", "mute"], help="Select the operation to perform")
-    parser.add_argument("files", nargs="+", help="List of video files to edit")
+    print('***********Welcome to Kevin Lukes simple Python-based video editor.************\n')
+    print('This video editor uses MoviePy library which itself is built upon FFmpeg.\n')
+    print('My code provides a clean wrapper to enable various video editing options from MoviePy.\n')
     
-    args = parser.parse_args()
+    operation = input("Select the operation to perform (clip, resize, merge, flip, mute): ").lower()
+    
+    file_list = input("Enter a list of video files separated by spaces: ").split()
+    
+    if operation == "clip":
+        clip_videos(file_list)
 
-    if args.operation == "clip":
-        print('Enter start and end times for clipping:')
-        start_time = int(input())
-        end_time = int(input())
-        clip_videos(args.files, start_time, end_time)
+    elif operation == "resize":
+        resize_videos(file_list)
 
-    elif args.operation == "resize":
-        print('Enter the height for resizing:')
-        height_val = int(input())
-        resize_videos(args.files, height_val)
+    elif operation == "merge":
+        merge_videos(file_list)
 
-    elif args.operation == "merge":
-        merge_videos(args.files)
+    elif operation == "flip":
+        flip_videos(file_list)
 
-    elif args.operation == "flip":
-        print('Enter the flip angle:')
-        flip_angle = int(input())
-        flip_videos(args.files, flip_angle)
-
-    elif args.operation == "mute":
-        mute_videos(args.files)
+    elif operation == "mute":
+        mute_videos(file_list)
 
 if __name__ == "__main__":
     main()
